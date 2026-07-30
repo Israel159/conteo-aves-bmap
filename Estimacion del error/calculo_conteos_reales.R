c5a404909afb5faea5d6f2d2c0089dab3b84d6da
Renzo <renzotovar493@gmail.com>
2026-07-30 11:55:16 -0500

    añadiendo el codigo del entrenamiento

---
+++ Estimacion del error/calculo_conteos_reales.R
@@ -0,0 +1,97 @@
+library(tidyr)
+library(dplyr)
+library(jsonlite)
+
+# Diccionario completo de especies
+diccionario_especies <- c(
+  "AMOY"  = "Ostrero americano",
+  "BFBO"  = "Piquero de patas azules",
+  "BLOY"  = "Ostrero negro",
+  "BLVU"  = "Gallinazo de cabeza negra",
+  "BROPE" = "pelicano",
+  "CHAL"  = "Chorlitejo patinegro",
+  "CHUCO" = "Chuita",
+  "FRGU"  = "Gaviota de Franklin",
+  "GRGU"  = "Garuma",
+  "GUCO"  = "Guanay",
+  "HUPE"  = "pinguino",
+  "INTE"  = "zarcillo",
+  "KEGU"  = "Gaviota dominicana",
+  "NECO"  = "cushuri",
+  "PEBO"  = "piquero",
+  "PEGU"  = "Gaviota peruana",
+  "PEFA"  = "Halcón peregrino",
+  "PEPE"  = "pelicano",
+  "PLAMA" = "Playerito manchado",
+  "RYNI"  = "Rayador",
+  "SNDL"  = "Playerito blanco",
+  "SWPL"  = "Chorlo nevado",
+  "THAEL" = "Gaviotín elegante",
+  "THAMA" = "Gaviotín real",
+  "THASA" = "Gaviotín de Sandwich",
+  "TUVU"  = "gallinazo cabeza roja",
+  "VEFLY" = "Turtupilín",
+  "YCNH"  = "Huaco de corona amarilla",
+  "ZATRI" = "Zarapito trinador"
+)
+
+
+#RLOF Abril 2025-------------------------------------------------
+
+##(Exte Inte afue)----------------------------------------------------
+datos <- read.delim("clipboard")
+#Solo fotos 119,124,127
+exte_inte_afue_abril_2025 <- datos %>% filter(FOTO==119|FOTO==124|FOTO==127)
+totales <- exte_inte_afue_abril_2025 %>% group_by(FOTO,ESPECIE) %>% summarise(Adultos=sum(ADULTOS,na.rm = T),
+                                                                              Juveniles=sum(JUVENILES,na.rm=T))
+
+# Transformamos a formato largo para el modelo
+totales_1 <- totales %>%
+  pivot_longer(
+    cols = c(Adultos, Juveniles),
+    names_to = "Edad",
+    values_to = "Real"
+  ) %>%
+  mutate(
+    # Nombre base de la especie
+    nombre_base = diccionario_especies[ESPECIE],
+    # Clase final: "Especie adulto" o "Especie juvenil"
+    Clase = ifelse(Edad == "Adultos",
+                   paste(nombre_base, "adulto"),
+                   paste(nombre_base, "juvenil")),
+  ) %>%
+  # Mantener los ceros para evaluar también las ausencias (recomendado)
+  select(FOTO,Clase, Real) %>% filter(Real>0)
+
+
+
+## Exte aden-----------------
+datos <- read.delim("clipboard")
+exte_aden_abril_2025 <- datos %>% filter(LADO=="EXTE ADEN")
+totales_exte_aden_abril_2025 <- exte_aden_abril_2025 %>% group_by(FOTO,ESPECIE)%>% summarise(Adultos=sum(ADULTOS,na.rm = T),
+                                                                          Juveniles=sum(JUVENILES,na.rm=T))
+totales_2 <- totales_exte_aden_abril_2025 %>%
+  pivot_longer(
+    cols = c(Adultos, Juveniles),
+    names_to = "Edad",
+    values_to = "Real"
+  ) %>%
+  mutate(
+    # Nombre base de la especie
+    nombre_base = diccionario_especies[ESPECIE],
+    # Clase final: "Especie adulto" o "Especie juvenil"
+    Clase = ifelse(Edad == "Adultos",
+                   paste(nombre_base, "adulto"),
+                   paste(nombre_base, "juvenil")),
+  ) %>%
+  select(FOTO,Clase, Real) %>% filter(Real>0)
+
+
+abril_2025_RLOF <- rbind(totales_1,totales_2)
+
+writexl::write_xlsx(abril_2025_RLOF,"abril_2025_RLOF.xlsx")
+
+#Rompeolas abril 2025 -------------------------
+datos <- read.delim("clipboard")
+
+
